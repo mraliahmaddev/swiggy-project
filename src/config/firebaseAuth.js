9230c1bff827e726b8676c9c5a91ef36bea0704e
Ali Ahmad <mr.aliahmaddev@gmail.com>
2025-04-09 20:45:40 +0500

    firebaseconfig

--- src/config/firebaseAuth.js
+++ src/config/firebaseAuth.js
@@ -8,7 +8,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // console.log(import.meta.env.VITE_KEY);
 
-const firebaseConfig = JSON.parse(import.meta.env.VITE_KEY);
+const firebaseConfig = {
+  "apiKey":import.meta.env.VITE_APIKEY,
+  "authDomain":import.meta.env.VITE_AUTHDOMAIN,
+  "projectId":import.meta.env.VITE_PROJECTID,
+  "storageBucket":import.meta.env.VITE_STORAGEBUCKET,
+  "messagingSenderId":import.meta.env.VITE_MESSAGINGSENDERID,
+  "appId":import.meta.env.VITE_APPID
+}
 
 
 
